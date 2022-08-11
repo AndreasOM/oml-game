6d325291354b8dfe9d068eae791322cf2a041cda
Andreas Neukoetter <andreas@omni-mad.com>
2022-08-11 20:40:02 +0200

    + [episode-0008] Add correct template name filling for digits.

--- src/renderer/animated_texture.rs
+++ src/renderer/animated_texture.rs
@@ -1,10 +1,13 @@
 use crate::renderer::{Renderer, Texture};
 use crate::system::System;
 
+use regex::Regex;
+
+use std::borrow::Cow;
+
 #[derive(Debug)]
 pub struct AnimatedTextureConfiguration {
-	pub prefix:           String,
-	pub number_of_digits: i8,
+	pub template:           String,
 	pub first_frame:      u16,
 	pub last_frame:       u16,
 	pub fps:              f32,
@@ -12,15 +15,14 @@ pub struct AnimatedTextureConfiguration {
 
 impl AnimatedTextureConfiguration {
 	pub fn new(
-		prefix: &str,
+		template: &str,
 		number_of_digits: i8,
 		first_frame: u16,
 		last_frame: u16,
 		fps: f32,
 	) -> Self {
 		Self {
-			prefix: prefix.to_owned(),
-			number_of_digits,
+			template: template.to_owned(),
 			first_frame,
 			last_frame,
 			fps,
@@ -31,8 +33,7 @@ impl AnimatedTextureConfiguration {
 impl From<(&str, i8, u16, u16, f32)> for AnimatedTextureConfiguration {
 	fn from(t: (&str, i8, u16, u16, f32)) -> Self {
 		Self {
-			prefix:           t.0.to_owned(),
-			number_of_digits: t.1,
+			template:           t.0.to_owned(),
 			first_frame:      t.2,
 			last_frame:       t.3,
 			fps:              t.4,
@@ -42,8 +43,7 @@ impl From<(&str, i8, u16, u16, f32)> for AnimatedTextureConfiguration {
 
 #[derive(Debug)]
 pub struct AnimatedTexture {
-	prefix:                String,
-	number_of_digits:      i8,
+	template:                String,
 	first_frame:           u16,
 	number_of_frames:      u16,
 	fps:                   f32,
@@ -57,8 +57,7 @@ pub struct AnimatedTexture {
 impl AnimatedTexture {
 	pub fn new() -> Self {
 		Self {
-			prefix:                String::new(),
-			number_of_digits:      0,
+			template:                String::new(),
 			first_frame:           0,
 			number_of_frames:      0,
 			fps:                   0.0,
@@ -72,14 +71,12 @@ impl AnimatedTexture {
 
 	pub fn setup(
 		&mut self,
-		prefix: &str,
-		number_of_digits: i8,
+		template: &str,
 		first_frame: u16,
 		number_of_frames: u16,
 		fps: f32,
 	) {
-		self.prefix = prefix.to_owned();
-		self.number_of_digits = number_of_digits;
+		self.template = template.to_owned();
 		self.first_frame = first_frame;
 		self.number_of_frames = number_of_frames;
 		self.fps = fps;
@@ -88,8 +85,7 @@ impl AnimatedTexture {
 	}
 
 	pub fn setup_from_config(&mut self, config: &AnimatedTextureConfiguration) {
-		self.prefix = config.prefix.clone();
-		self.number_of_digits = config.number_of_digits;
+		self.template = config.template.clone();
 		self.first_frame = config.first_frame;
 		self.number_of_frames = config.last_frame - config.first_frame;
 		self.fps = config.fps;
@@ -140,30 +136,47 @@ impl AnimatedTexture {
 
 	pub fn r#use(&self, renderer: &mut Renderer) {
 		//		dbg!(&self);
-		let name = AnimatedTexture::append_number_with_digits(
-			&self.prefix,
+		let name = AnimatedTexture::fill_template(
+			&self.template,
 			self.current_frame,
-			self.number_of_digits,
 		);
 		renderer.use_texture(&name)
 	}
 
-	fn append_number_with_digits(prefix: &str, number: u16, number_of_digits: i8) -> String {
-		match number_of_digits {
-			0 => {
-				format!("{}", &prefix)
-			},
-			2 => {
-				format!("{}{:02}", &prefix, number)
-			},
-			n if n < 0 => {
-				format!("{}{}", &prefix, number)
-			},
-			4 => {
-				format!("{}{:04}", &prefix, number)
-			},
-			_ => todo!("Add support for {} digits", number_of_digits),
-		}
+	// pub for easier testing
+	pub fn fill_template( template: &str, number: u16 ) -> Cow<str> {
+		let re = Regex::new(r"(.*)%((0*\d+)*)d(.*)").unwrap();
+		for caps in re.captures_iter(template) {
+//			dbg!(&caps);
+			if caps.len() == 5 {
+				let (prefix,format,suffix ) = (&caps[1], &caps[2], &caps[4]);
+//				eprintln!("{:?} {:?} {:?}", &prefix, &format, &suffix );
+				let r = match format {
+					// :TODO: replace via macros?
+					"" => format!( "{}{}{}", &prefix, number, &suffix),
+					"01" => format!( "{}{:01}{}", &prefix, number, &suffix),
+					"02" => format!( "{}{:02}{}", &prefix, number, &suffix),
+					"03" => format!( "{}{:03}{}", &prefix, number, &suffix),
+					"04" => format!( "{}{:04}{}", &prefix, number, &suffix),
+					"05" => format!( "{}{:05}{}", &prefix, number, &suffix),
+					"06" => format!( "{}{:06}{}", &prefix, number, &suffix),
+					"07" => format!( "{}{:07}{}", &prefix, number, &suffix),
+					"08" => format!( "{}{:08}{}", &prefix, number, &suffix),
+					"1" => format!( "{}{:1}{}", &prefix, number, &suffix),
+					"2" => format!( "{}{:2}{}", &prefix, number, &suffix),
+					"3" => format!( "{}{:3}{}", &prefix, number, &suffix),
+					"4" => format!( "{}{:4}{}", &prefix, number, &suffix),
+					"5" => format!( "{}{:5}{}", &prefix, number, &suffix),
+					"6" => format!( "{}{:6}{}", &prefix, number, &suffix),
+					"7" => format!( "{}{:7}{}", &prefix, number, &suffix),
+					"8" => format!( "{}{:8}{}", &prefix, number, &suffix),
+					_ => format!( "{}BROKEN_TEMPLATE_%{}d_{}", &prefix, &format, &suffix),
+				};
+				return r.into();
+//				dbg!(&caps);
+			}
+		};
+		template.into()
 	}
 
 	pub fn set_completed(&mut self, completed: bool) {
@@ -177,8 +190,7 @@ impl AnimatedTexture {
 	pub fn register_all(
 		system: &mut System,
 		renderer: &mut Renderer,
-		prefix: &str,
-		number_of_digits: i8,
+		template: &str,
 	) -> usize {
 		let fs = system.default_filesystem_mut();
 
@@ -186,8 +198,7 @@ impl AnimatedTexture {
 		let mut i = 0;
 
 		loop {
-			//			let name = format!( &template, i );	// :(
-			let name = AnimatedTexture::append_number_with_digits(prefix, i, number_of_digits);
+			let name = AnimatedTexture::fill_template(template, i);
 
 			// :HACK: to workaround missing "exists with .*"
 			let name_ext = format!("{}.png", &name);
@@ -209,5 +220,34 @@ impl AnimatedTexture {
 
 		//		todo!("die");
 		to_load.len()
+	}
+}
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+	#[test]
+	fn template_filenames_work() -> anyhow::Result<()> {
+		assert_eq!("test-0012-test", AnimatedTexture::fill_template( "test-0012-test", 12 ) );
+// NO!		assert_eq!("test-12-12-test", AnimatedTexture::fill_template( "test-%01d-%01d-test", 12 ) );
+		assert_eq!("test-12-test", AnimatedTexture::fill_template( "test-%01d-test", 12 ) );
+		assert_eq!("test-12-test", AnimatedTexture::fill_template( "test-%02d-test", 12 ) );
+		assert_eq!("test-012-test", AnimatedTexture::fill_template( "test-%03d-test", 12 ) );
+		assert_eq!("test-0012-test", AnimatedTexture::fill_template( "test-%04d-test", 12 ) );
+		assert_eq!("test-00012-test", AnimatedTexture::fill_template( "test-%05d-test", 12 ) );
+		assert_eq!("test-000012-test", AnimatedTexture::fill_template( "test-%06d-test", 12 ) );
+		assert_eq!("test-0000012-test", AnimatedTexture::fill_template( "test-%07d-test", 12 ) );
+		assert_eq!("test-00000012-test", AnimatedTexture::fill_template( "test-%08d-test", 12 ) );
+		assert_eq!("test-12-test", AnimatedTexture::fill_template( "test-%1d-test", 12 ) );
+		assert_eq!("test-12-test", AnimatedTexture::fill_template( "test-%2d-test", 12 ) );
+		assert_eq!("test- 12-test", AnimatedTexture::fill_template( "test-%3d-test", 12 ) );
+		assert_eq!("test-  12-test", AnimatedTexture::fill_template( "test-%4d-test", 12 ) );
+		assert_eq!("test-   12-test", AnimatedTexture::fill_template( "test-%5d-test", 12 ) );
+		assert_eq!("test-    12-test", AnimatedTexture::fill_template( "test-%6d-test", 12 ) );
+		assert_eq!("test-     12-test", AnimatedTexture::fill_template( "test-%7d-test", 12 ) );
+		assert_eq!("test-      12-test", AnimatedTexture::fill_template( "test-%8d-test", 12 ) );
+		assert_eq!("test-12-test", AnimatedTexture::fill_template( "test-%d-test", 12 ) );
+		assert_eq!("test-65535-test", AnimatedTexture::fill_template( "test-%d-test", 65535 ) );
+		Ok(())
 	}
 }
