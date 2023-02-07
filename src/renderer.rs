b330a818f069bab2f2e36b68033228499c7dcac6
Andreas Neukoetter <andreas@omni-mad.com>
2023-02-07 19:50:03 +0100

    ! [episode-0035] Do not skip first screenshot.

--- src/renderer.rs
+++ src/renderer.rs
@@ -1054,7 +1054,7 @@ impl Renderer {
 	pub(crate) fn update_queued_screenshots(&mut self) -> Vec<String> {
 		let mut r = Vec::new();
 		let mut to_remove = Vec::new();
-		for i in 1..self.queued_screenshots.len() {
+		for i in 0..self.queued_screenshots.len() {
 			let mut qs = &mut self.queued_screenshots[i];
 			qs.delay = qs.delay.saturating_sub(1);
 			if qs.delay <= 0 {
